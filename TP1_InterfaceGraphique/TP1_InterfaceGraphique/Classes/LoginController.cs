ea7c4df1da7dc747542dfee6f87f51893c6253d4
KatsuroDev <katsuro.userg@gmail.com>
2021-11-04 15:15:28 -0400

    add Login window, login controller and admin panel
    Login is now working you can log as a teacher or a student.

---
+++ TP1_InterfaceGraphique/TP1_InterfaceGraphique/Classes/LoginController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_InterfaceGraphique.Classes
+{
+    public class LoginController
+    {
+        public static User ActiveUser;
+
+        public static bool TryLogin(int userId, string password, bool isStudent)
+        {
+            if (isStudent)
+                return TryLoginStudent(userId, password);
+            else
+                return TryLoginTeacher(userId, password);
+        }
+
+        private static bool TryLoginStudent(int userId, string password)
+        {
+            var students = App.Current.Students;
+
+            Student student = null;
+
+            if (students.TryGetValue(userId, out student))
+            {
+                if (AreCredentialsValid(student, password))
+                {
+                    ActiveUser = student;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool TryLoginTeacher(int userId, string password)
+        {
+            var teachers = App.Current.Teachers;
+
+            Teacher teacher = null;
+
+            if (teachers.TryGetValue(userId, out teacher))
+            {
+                if (AreCredentialsValid(teacher, password))
+                {
+                    ActiveUser = teacher;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool AreCredentialsValid(User user, string password)
+        {
+            return user.Password == password;
+        }
+    }
+}
--- TP1_InterfaceGraphique/TP1_InterfaceGraphique/Windows/LogIn.xaml.cs
+++ TP1_InterfaceGraphique/TP1_InterfaceGraphique/Windows/LogIn.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TP1_InterfaceGraphique.Classes;
 
 namespace TP1_InterfaceGraphique.Windows
 {
@@ -22,6 +23,32 @@ namespace TP1_InterfaceGraphique.Windows
         public LogIn()
         {
             InitializeComponent();
+        }
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+            bool? isStudent = StudentRdBtn.IsChecked;
+
+            String usernameInput = UsernameTxtBox.Text;
+            String passwordInput = PasswordTxtBox.Password;
+
+
+            if (isStudent == null)
+                throw new NullReferenceException();
+
+
+            int userId;
+            if (!Int32.TryParse(usernameInput, out userId))
+            {
+                MessageBox.Show($"L'identifiant ou le mot de passe est invalide.", "Erreur d'authentification", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+            if (!LoginController.TryLogin(userId, passwordInput, isStudent.Value))
+            {
+                MessageBox.Show($"L'identifiant ou le mot de passe est invalide.", "Erreur d'authentification", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
